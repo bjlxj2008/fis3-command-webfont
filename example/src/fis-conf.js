0c98449cda9ea71f6a631070cddb4e8bac66fa84
lwdos <1127622940@qq.com>
2015-09-02 23:04:08 +0800

    add denpendicy & example

---
+++ example/src/fis-conf.js
@@ -0,0 +1,6 @@
+fis.config.set("webfont",{
+    'src'       : '**.svg',//图标目录
+    'dest'      : '/fonts',  //产出字体目录
+    'fontname'  : 'reasy_font', //产出字体名称
+    'order'     : 'name' //name或者time //图标按名称还是按修改时间排序，默认按名称排序
+});
